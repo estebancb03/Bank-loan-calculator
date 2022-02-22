d86be3258a3bf00c2f158f7527552e6086ad3da9
estebancb03 <estebanr.castaneda03@gmail.com>
2022-02-21 23:50:15 -0600

    Form validations

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Form = props => {
     const { amount, saveAmount, termToPay, saveTermToPay } = props;
@@ -8,43 +8,48 @@ const Form = props => {
         e.preventDefault();
         if(amount === 0 || termToPay === '') {
             saveError(true);
-        }   
+            return;
+        }
+        saveError(false);   
     }
 
     return (  
-        <form onSubmit={ calculateLoan }>
-            <div className="row">
-                <div>
-                    <label>Cantidad Prestamo</label>
-                    <input 
-                        className="u-full-width" 
-                        type="number" 
-                        placeholder="Ejemplo: 3000"
-                        onChange={ e => saveAmount(parseInt(e.target.value)) } 
-                    />
+        <Fragment>
+            <form onSubmit={ calculateLoan }>
+                <div className="row">
+                    <div>
+                        <label>Cantidad Prestamo</label>
+                        <input 
+                            className="u-full-width" 
+                            type="number" 
+                            placeholder="Ejemplo: 3000"
+                            onChange={ e => saveAmount(parseInt(e.target.value)) } 
+                        />
+                    </div>
+                    <div>
+                        <label>Plazo para Pagar</label>
+                        <select 
+                            className="u-full-width"
+                            onChange={ e => saveTermToPay(parseInt(e.target.value)) }
+                        >
+                            <option value="">Seleccionar</option>
+                            <option value="3">3 meses</option>
+                            <option value="6">6 meses</option>
+                            <option value="12">12 meses</option>
+                            <option value="24">24 meses</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input 
+                            type="submit" 
+                            value="Calcular" 
+                            className="button-primary u-full-width" 
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Plazo para Pagar</label>
-                    <select 
-                        className="u-full-width"
-                        onChange={ e => saveTermToPay(parseInt(e.target.value)) }
-                    >
-                        <option value="">Seleccionar</option>
-                        <option value="3">3 meses</option>
-                        <option value="6">6 meses</option>
-                        <option value="12">12 meses</option>
-                        <option value="24">24 meses</option>
-                    </select>
-                </div>
-                <div>
-                    <input 
-                        type="submit" 
-                        value="Calcular" 
-                        className="button-primary u-full-width" 
-                    />
-                </div>
-            </div>
-        </form>
+            </form>
+            { error ? <p className='error'>Todos los campos son obligatorios</p> : null }
+        </Fragment>
     );
 }
  
