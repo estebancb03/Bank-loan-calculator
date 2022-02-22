8c6289b1165717d6c22badee5ad523e1128969f8
estebancb03 <estebanr.castaneda03@gmail.com>
2022-02-21 23:44:23 -0600

    Read form data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 
 function App() {
+  const [amount, saveAmount] = useState(0);
+  const [termToPay, saveTermToPay] = useState('');
+
   return (
     <Fragment>
       <Header 
         title="Cotizador de prestamos"
       />
       <div className="container">
-        <Form />
+        <Form 
+          amount={ amount }
+          saveAmount={ saveAmount }
+          termToPay={ termToPay }
+          saveTermToPay={ saveTermToPay }
+        />
       </div>
     </Fragment>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => {
+const Form = props => {
+    const { amount, saveAmount, termToPay, saveTermToPay } = props;
+    const [error, saveError] = useState(false);
+
+    const calculateLoan = e => {
+        e.preventDefault();
+        if(amount === 0 || termToPay === '') {
+            saveError(true);
+        }   
+    }
+
     return (  
-        <form>
-          <div className="row">
-              <div>
-                  <label>Cantidad Prestamo</label>
-                  <input 
-                      className="u-full-width" 
-                      type="number" 
-                      placeholder="Ejemplo: 3000" 
-                  />
-              </div>
-              <div>
-                  <label>Plazo para Pagar</label>
-                  <select 
-                      className="u-full-width"
-                  >
-                      <option value="">Seleccionar</option>
-                      <option value="3">3 meses</option>
-                      <option value="6">6 meses</option>
-                      <option value="12">12 meses</option>
-                      <option value="24">24 meses</option>
-                  </select>
-              </div>
-              <div>
-                  <input 
-                      type="submit" 
-                      value="Calcular" 
-                      className="button-primary u-full-width" 
-                  />
-              </div>
-          </div>
+        <form onSubmit={ calculateLoan }>
+            <div className="row">
+                <div>
+                    <label>Cantidad Prestamo</label>
+                    <input 
+                        className="u-full-width" 
+                        type="number" 
+                        placeholder="Ejemplo: 3000"
+                        onChange={ e => saveAmount(parseInt(e.target.value)) } 
+                    />
+                </div>
+                <div>
+                    <label>Plazo para Pagar</label>
+                    <select 
+                        className="u-full-width"
+                        onChange={ e => saveTermToPay(parseInt(e.target.value)) }
+                    >
+                        <option value="">Seleccionar</option>
+                        <option value="3">3 meses</option>
+                        <option value="6">6 meses</option>
+                        <option value="12">12 meses</option>
+                        <option value="24">24 meses</option>
+                    </select>
+                </div>
+                <div>
+                    <input 
+                        type="submit" 
+                        value="Calcular" 
+                        className="button-primary u-full-width" 
+                    />
+                </div>
+            </div>
         </form>
     );
 }
